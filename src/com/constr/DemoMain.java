a572329b36e743517e699b64d87b0c0ce6ad905b
Sania2001 <saniagshinde@gmail.com>
2022-03-15 21:13:46 +0530

    Parametarized

---
+++ src/com/constr/DemoMain.java
@@ -0,0 +1,13 @@
+package com.constr;
+
+import org.w3c.dom.ls.LSOutput;
+
+import java.sql.SQLOutput;
+
+public class DemoMain {
+    public static void main(String[] args) {
+        DemoSubclass demo = new DemoSubclass();
+        System.out.println(demo.x);
+    }
+
+}
---
+++ src/com/constr/DemoSubclass.java
@@ -0,0 +1,13 @@
+package com.constr;
+
+public class DemoSubclass {
+    int x;
+
+    public DemoSubclass(int x) {
+        this.x = x;
+    }
+
+    public DemoSubclass(){
+        x = 120;
+    }
+}
